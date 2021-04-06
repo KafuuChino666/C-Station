f6ee1323bdd5d5267fe04092471b7095618363c7
CalebCX <690209522@qq.com>
2021-04-06 23:16:27 +0800

    添加注释以及其他
    添加了表功能的注释，创建字段时间，修改字段时间，新增积分表和硬币表，去除部分表（原货币部分）。

--- database/Database Design.sql
+++ database/Database Design.sql
@@ -1,23 +1,29 @@
+
+#用户表
 CREATE TABLE tb_user (
 	user_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	user_name VARCHAR(24),
+	user_name VARCHAR(24) NOT NULL,
 	user_sign VARCHAR(255),
-	gender CHAR(1) DEFAULT'M',
-	userc_time DATETIME,
-	birth DATE
+	gender CHAR(1) DEFAULT'M' NOT NULL,
+	gmt_create DATETIME NOT NULL,
+	birth DATE,
+	gmt_modified DATETIME NOT NULL
 );
 #insert into user_tb(user_name, user_sign, gender, birth) values('qweqwe', 'qweqwe', 'W','2000-02-12');
 
+#图片表
 CREATE TABLE tb_img (
 	img_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	img_location VARCHAR(255),
 	user_id INT,
 	img_type INT,
-	img_time DATETIME,
+	gmt_create DATETIME,
+	gmt_modified DATETIME,
 	FOREIGN KEY(user_id) REFERENCES tb_user(user_id)
 );
 
 ########
+#账号安全表
 CREATE TABLE tb_safe (
 	safe_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	user_id INT,
@@ -25,42 +31,52 @@ CREATE TABLE tb_safe (
 	phone INT,
 	ep_id INT NOT NULL UNIQUE,
 	rn_id INT NOT NULL UNIQUE,
+	gmt_create DATETIME NOT NULL,
+	gmt_modified DATETIME NOT NULL,
 	FOREIGN KEY(user_id) REFERENCES tb_user(user_id)
 );
 
+#密保问题表
 CREATE TABLE tb_pb (
 	ep_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	problem VARCHAR(255) NOT NULL,
 	answer VARCHAR(18) NOT NULL,
-	pbc_time DATETIME,
+	gmt_create DATETIME NOT NULL,
+	gmt_modified DATETIME NOT NULL,
 	FOREIGN KEY(ep_id) REFERENCES tb_safe(ep_id)
 );
 
+#实名表
 CREATE TABLE tb_realname (
 	rn_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	realname VARCHAR(20),
-	ID_number VARCHAR(18),
-	rn_time DATETIME
+	realname VARCHAR(20) nnot NULL,
+	ID_number VARCHAR(18) NOT NULL,
+	gmt_create DATETIME NOT NULL,
+	gmt_modified DATETIME NOT NULL,
 	FOREIGN KEY(rn_id) REFERENCES tb_safe(rn_id)
 );
 
 ####
-
-CREATE TABLE tb_vip_total (
-	vp_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	total_id INT NOT NULL UNIQUE,
-	coin_id INT NOT NULL UNIQUE,
+#积分表
+CREATE TABLE tb_integral(
+	integral_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	user_id INT,
+	ig_balance INT NOT NULL,
+	modify_cause VARCHAR NOT NULL,
+	change_number INT NOT NULL,
+	gmt_create DATETIME NOT NULL,
+	gmt_modified DATETIME NOT NULL,
 	FOREIGN KEY(user_id) REFERENCES tb_user(user_id)
-);
+)
 
-CREATE TABLE tb_total (
-	total_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	total_balance FLOAT NOT NULL,
-	consumption_id INT NOT NULL UNIQUE,
-	FOREIGN KEY(total_id) REFERENCES tb_vip_total(total_id)
-);
-
-CREATE TABLE tb_consumption (
-	consumption_id
-);
+#硬币表
+CREATE TABLE tb_coin(
+	coin_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	user_id INT,
+	coin_balance INT NOT NULL, 
+	modify_cause VARCHAR NOT NULL,
+	change_number INT NOT NULL,
+	gmt_create DATETIME NOT NULL,
+	gmt_modified DATETIME NOT NULL,
+	FOREIGN KEY(user_id) REFERENCES tb_user(user_id)
+)
