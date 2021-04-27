027aa9e46a380be96c8a64ee9346f5165885b37e
CalebCX <690209522@qq.com>
2021-04-27 18:30:31 +0800

    番剧表优化

--- database/Database Design_bangumi.sql
+++ database/Database Design_bangumi.sql
@@ -1,9 +1,8 @@
-# 番剧表bg开头
+# 番剧表 bg开头
 CREATE TABLE bg_bangumi_indexs(
 	bangumi_id INT PRIMARY KEY AUTO_INCREMENT,
 	bangumi_type VARCHAR(32) COMMENT '番剧类型' NOT NULL,
 	bangumi_info_id INT NOT NULL UNIQUE,
-	bangumi_status_id INT UNIQUE NOT NULL,
 	gmt_create DATETIME COMMENT '创建时间' NOT NULL,
 	gmt_modified DATETIME COMMENT '修改时间' NOT NULL
 );
@@ -14,7 +13,7 @@ CREATE TABLE bg_bangumi_info(
 	bangumi_info_id INT PRIMARY KEY AUTO_INCREMENT,
 	bangumi_name VARCHAR(32) NOT NULL COMMENT '番剧名称' NOT NULL,
 	bangumi_info_str TEXT NOT NULL COMMENT '番剧简介信息' NOT NULL,
-	bangumi_type VARCHAR(20) COMMENT '番剧状态' NOT NULL,
+	bangumi_status VARCHAR(20) COMMENT '番剧状态' NOT NULL,
 	sub_bangumi_id INT,
 	video_id INT,
 	gmt_create DATETIME COMMENT '创建时间' NOT NULL,
