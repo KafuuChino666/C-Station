59d7bf3df31e94be9a88712e120ea6018c868967
mcsql0 <2697287132@qq.com>
2021-05-29 13:19:56 +0800

    bug修改

--- site/c_station_admin/src/router/index.js
+++ site/c_station_admin/src/router/index.js
@@ -182,7 +182,7 @@ export const constantRoutes = [
         children: [
           {
             path: 'user',
-            component: () => import('')
+            component: () => import('@/views/404')
           }
         ]
       },
--- site/c_station_admin/src/views/user/components/userList.vue
+++ site/c_station_admin/src/views/user/components/userList.vue
@@ -1,82 +1,89 @@
 <template>
   <div style="width: 80%; margin: auto">
-  <el-table
-    :data="userData"
-    style="width: 100%">
-    <el-table-column type="expand">
-      <template slot-scope="props">
-        <el-form label-position="left" inline class="demo-table-expand">
-          <el-form-item label="用户ID">
-            <span>{{ props.row.userId}}</span>
-          </el-form-item>
-          <el-form-item label="用户昵称">
-            <span>{{ props.row.userName }}</span>
-          </el-form-item>
-          <el-form-item label="真实姓名">
-            <span>{{ props.row.realName }}</span>
-          </el-form-item>
-          <el-form-item label="性别">
-            <span>{{ props.row.gender }}</span>
-          </el-form-item>
-          <el-form-item label="用户分类">
-            <span>{{ props.row.category }}</span>
-          </el-form-item>
-          <el-form-item label="用户手机号">
-            <span>{{ props.row.phone }}</span>
-          </el-form-item>
-          <el-form-item label="邮箱">
-            <span>{{ props.row.email }}</span>
-          </el-form-item>
-          <el-form-item label="身份证号">
-            <span>{{ props.row.idNumber }}</span>
-          </el-form-item>
-          <el-form-item label="充值数">
-            <span>{{ props.row.consumed }}</span>
-          </el-form-item>
-        </el-form>
-      </template>
-    </el-table-column>
-    <el-table-column
-      align="center"
-      label="用户ID"
-      prop="userId">
-    </el-table-column>
-    <el-table-column
-      align="center"
-      label="用户昵称"
-      prop="userName">
-    </el-table-column>
-    <el-table-column
-      align="center"
-      label="性别"
-      prop="gender">
-    </el-table-column>
-    <el-table-column
-      align="center"
-      label="邮箱"
-      prop="email">
-    </el-table-column>
-    <el-table-column
-      align="center"
-      label="查看">
-      <template slot-scope="scope">
-        <el-row>
-          <el-button type="info" size="mini" @click="userMain()" round>查看主页</el-button>
-          <el-button type="warning" size="mini" @click="userViolation()" round>违规记录</el-button>
-        </el-row>
-      </template>
-    </el-table-column>
-    <el-table-column
-      align="center"
-      label="操作">
-      <template slot-scope="scope">
-        <el-row>
-          <el-button type="primary" size="mini" @click="userRedact()" round>用户编辑</el-button>
-          <el-button type="danger" size="mini" @click="userBan()" round>封禁用户</el-button>
-        </el-row>
-      </template>
-    </el-table-column>
-  </el-table>
+    <el-table
+      :data="userData"
+      style="width: 100%"
+    >
+      <el-table-column type="expand">
+        <template slot-scope="props">
+          <el-form label-position="left" inline class="demo-table-expand">
+            <el-form-item label="用户ID">
+              <span>{{ props.row.userId }}</span>
+            </el-form-item>
+            <el-form-item label="用户昵称">
+              <span>{{ props.row.userName }}</span>
+            </el-form-item>
+            <el-form-item label="真实姓名">
+              <span>{{ props.row.realName }}</span>
+            </el-form-item>
+            <el-form-item label="性别">
+              <span>{{ props.row.gender }}</span>
+            </el-form-item>
+            <el-form-item label="用户分类">
+              <span>{{ props.row.category }}</span>
+            </el-form-item>
+            <el-form-item label="用户手机号">
+              <span>{{ props.row.phone }}</span>
+            </el-form-item>
+            <el-form-item label="邮箱">
+              <span>{{ props.row.email }}</span>
+            </el-form-item>
+            <el-form-item label="身份证号">
+              <span>{{ props.row.idNumber }}</span>
+            </el-form-item>
+            <el-form-item label="充值数">
+              <span>{{ props.row.consumed }}</span>
+            </el-form-item>
+          </el-form>
+        </template>
+      </el-table-column>
+      <el-table-column
+        align="center"
+        label="用户ID"
+        prop="userId"
+      >
+      </el-table-column>
+      <el-table-column
+        align="center"
+        label="用户昵称"
+        prop="userName"
+      >
+      </el-table-column>
+      <el-table-column
+        align="center"
+        label="性别"
+        prop="gender"
+      >
+      </el-table-column>
+      <el-table-column
+        align="center"
+        label="邮箱"
+        prop="email"
+      >
+      </el-table-column>
+      <el-table-column
+        align="center"
+        label="查看"
+      >
+        <template slot-scope="scope">
+          <el-row>
+            <el-button type="info" size="mini" @click="userMain()" round>查看主页</el-button>
+            <el-button type="warning" size="mini" @click="userViolation()" round>违规记录</el-button>
+          </el-row>
+        </template>
+      </el-table-column>
+      <el-table-column
+        align="center"
+        label="操作"
+      >
+        <template slot-scope="scope">
+          <el-row>
+            <el-button type="primary" size="mini" @click="userRedact()" round>用户编辑</el-button>
+            <el-button type="danger" size="mini" @click="userBan()" round>封禁用户</el-button>
+          </el-row>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -125,12 +132,12 @@ export default {
       this.$router.push('')
     },
 
-    //用户编辑
+    // 用户编辑
     userRedact() {
       this.$router.push('')
     },
 
-    //用户封禁
+    // 用户封禁
     userBan() {
       this.$router.push('')
     }
@@ -142,10 +149,12 @@ export default {
 .demo-table-expand {
   font-size: 0;
 }
+
 .demo-table-expand label {
   width: 90px;
   color: #99a9bf;
 }
+
 .demo-table-expand .el-form-item {
   margin-right: 0;
   margin-bottom: 0;
