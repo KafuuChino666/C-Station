beb9fdc67779a6d1801b34668fa136a8e93542cb
CalebCX <690209522@qq.com>
2021-05-30 20:45:16 +0800

    ECharts用户统计表实验

---
+++ site/c_station_admin/src/views/user/components/userStat.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <div class="pie">
+      <div id="pie1">
+        <!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
+        <div id="main1" style="float:left;width:100%;height: 300px" />
+      </div>
+      <div id="pie2">
+        <div id="main2" style="float:left;width:100%;height: 300px" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const echarts = require('echarts/lib/echarts')
+// 引入饼状图组件
+require('echarts/lib/chart/pie')
+// 引入提示框和title组件
+require('echarts/lib/component/tooltip')
+require('echarts/lib/component/title')
+export default {
+  name: 'UserStat',
+  mounted() {
+    this.initData();
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    initData() {
+      var myChart = echarts.init(document.getElementById('main1'))
+      myChart.setOption({
+        title: {
+          text: '某站点用户访问来源',
+          subtext: '纯属虚构',
+          x: 'center'
+        },
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        legend: {
+          orient: 'vertical',
+          bottom: 'bottom',
+          data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+        },
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+              {
+                value: 335,
+                name: '直接访问'
+              },
+              {
+                value: 310,
+                name: '邮件营销'
+              },
+              {
+                value: 234,
+                name: '联盟广告'
+              },
+              {
+                value: 135,
+                name: '视频广告'
+              },
+              {
+                value: 1548,
+                name: '搜索引擎'
+              }
+            ],
+            itemStyle: {
+              emphasis: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
